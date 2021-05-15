9333b2e38da03d91b3ba059be61e4757c39a3382
SantiRG <santiago.rodriguezg@perficient.com>
2021-05-14 21:19:44 -0500

    make progress into session number 24 of the Udemy course

--- cypress/integration/examples/Test12.js
+++ cypress/integration/examples/Test12.js
@@ -1,24 +1,19 @@
 /// <reference types="Cypress" />
 
-describe('My First Test Suite', () => {
+describe('My Second Test Suite', () => {
     it('My First Test Case', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
         cy.get('.search-keyword').type('ca')
         cy.wait(2000)
-        cy.get('.product').should('have.length', 5)
-        cy.get('.product:visible').should('have.length', 4)
         cy.get('.products').as('productLocator')
-        cy.get('@productLocator').find('.product').should('have.length', 4)
-        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
         cy.get('@productLocator').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('h4.product-name').text()
             if(textVeg.includes('Cashews')) {
                 $el.find('button').click()
             }
         })
-        cy.get('.brand').should('have.text', 'GREENKART')
-        cy.get('.brand').then(function(logoElement) {
-            cy.log(logoElement.text())
-        })
+        cy.get('.cart-icon > img').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
     })
 })
